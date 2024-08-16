c23949ec13fa845d008f138ea61bd8ca35dbd275
Wahab-dev09 <wahabzsec@gmail.com>
2024-08-16 04:18:37 -0600

    Initial Setup

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Routes>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
